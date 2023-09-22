1e7627e3ec9861cbb218027a26ce613e329b705e
Anselmo Del hoyo <anselmodelhoyo11@gmail.com>
2023-09-22 11:06:31 -0400

    Learn basic data types in Python

---
+++ Python-bases/tipos-de-datos/tipos-de-datos.py
@@ -0,0 +1,28 @@
+
+# String (texto).
+nombre = "Anselmo"
+
+# Int (número entero).
+edad = 18
+
+# Float (número con coma flotante)..
+numero_flotante = 1.79
+
+# String que permite saltos de línea.
+text = """
+  Hola, este es un texto
+  de más de una línea
+"""
+
+# print() : función para mostrar por consola un valor.
+print("Hola mundo")
+print(nombre)
+
+# Ejemplo de suma
+
+a = 2
+b = 3
+
+c = a + b
+
+print(c)
